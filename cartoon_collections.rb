7effe23b9c428680eefe956e4f8ca37efb05488f
oyayafelix <oyayafelix@gmail.com>
2022-08-20 14:57:18 +0300

    commit tree f11330d575c03ec1a2821ec63f0517413b164d46

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,48 +1,32 @@
-dwarves = %w[
-  Doc
-  Dopey
-  Bashful
-  Grumpy
-  Sneezy
-  Sleepy
-  Happy
-]
+def roll_call_dwarves dwarves
+  # Your code here
+  dwarves.each_with_index {|dwarf, index| puts "#{index+1}. #{dwarf}"}
+end
 
-def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |dwarf, index|
-    puts "#{index + 1}. #{dwarf}"
+def summon_captain_planet planeteer_calls
+  # Your code here
+  planeteer_calls.map do |planet|
+    planet.capitalize() + "!"
   end
 end
 
-roll_call_dwarves(dwarves)
-
-
-planeteer_calls = %w[earth wind fire water heart]
-
-
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map { |call| call.capitalize + '!' }
-end
-
-summon_captain_planet(planeteer_calls)
-
-
-
-def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? { |call| call.length > 4 }
-end
-
-long_planeteer_calls(planeteer_calls)
-
-
-potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
-
-def find_the_cheese(potentially_cheesy_items)
-  cheeses = %w[gouda cheddar camembert]
-
-  potentially_cheesy_items.find do |maybe_cheese|
-    cheeses.include?(maybe_cheese)
+def long_planeteer_calls words
+  # Your code here
+  words.each do |word|
+    if word.length>4
+      return true
+    end
   end
+  return false
 end
 
-find_the_cheese(potentially_cheesy_items)
+def find_the_cheese food
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  food.each do |ingredient|
+    if cheese_types.include?(ingredient) == true
+      return ingredient
+    end
+  end
+  nil
+end
